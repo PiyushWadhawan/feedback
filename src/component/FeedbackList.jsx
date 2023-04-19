80eda7910025c4ffc992ccd19cf1c3d748b373c5
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2023-04-19 18:56:45 +0530

    uploading to github hosting on netlify

---
+++ src/component/FeedbackList.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import FeedbackItem from './FeedbackItem'
+import {useContext} from 'react'
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackList() {
+  
+    const {feedback} = useContext(FeedbackContext)
+
+    if(!feedback || feedback.length === 0)
+    {
+        return <p>No Feedback Yet</p>
+    }
+
+    return (
+        <div className='feedback-list'>
+            <>
+            {feedback.map((item) => ( 
+                <>
+                    <FeedbackItem key={item.id} item={item} />
+                </>
+            ))}
+            </>
+        </div>
+      )
+
+//     return (
+//     <div className='feedback-list'>
+//         {feedback.map((item) => { 
+//             return <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+//         })}
+//     </div>
+//   )
+}
+
+export default FeedbackList
